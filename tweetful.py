0345b194f87da4eb2dce90d556a784a4731d5f3e
Jaime Raczka <jaime.raczka@gmail.com>
2014-08-20 15:58:15 -0400

    Includes Twitter API function

--- tweetful.py
+++ tweetful.py
@@ -1,12 +1,28 @@
+import requests
+import argparse
+import sys
+ 
 import authorization
-
+from urls import *
+ 
 def main():
     """ Main function """
     auth = authorization.authorize()
-
+ 
     response = requests.get(TIMELINE_URL, auth=auth)
-    print response.json()
-
+    print json.dumps(response.json(), indent=4)
+ 
 if __name__ == "__main__":
     main()
 
+# Sets up argparse input
+parser = argparse.ArgumentParser()
+parser.add_argument(username="Twitter Handle", help="enter the TWitter Handle you want data on",type=len)
+args = parser.parse_args()
+
+
+def twittertimeline():
+	""" Calls Twitter API to get most recent user mentions on Twitter """
+	timeline = https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=username
+
+    print timeline
